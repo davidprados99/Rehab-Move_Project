e0a5bf5e6c37c1198dac23d164e802db9f8ed889
davidprados99 <davidprados99@gmail.com>
2026-03-25 12:30:22 +0100

    Fix: Add SQLite support

--- backend/database.py
+++ backend/database.py
@@ -8,23 +8,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Build the database URL from environment variables
-SQLALCHEMY_DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+# Get context variables for database connection
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
-#sqlalchemy engine
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# If some variables are missing, we fall back to SQLite for testing purposes
 
-#SessionLocal class will be used to create database sessions
+if not all([DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME]):
+    print("Configuración DB no encontrada. Usando SQLite para tests")
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+    # Check same thread is needed for SQLite to avoid issues with multiple threads accessing the database
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    # If all variables are present, we connect to PostgreSQL
+    SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    print(f"Conectado a PostgreSQL: {DB_NAME}")
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-#Base class for our models
 Base = declarative_base()
-
-# Function to get a database session, we will use this in our API endpoints to interact with the database
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
